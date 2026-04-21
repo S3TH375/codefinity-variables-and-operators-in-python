8c57bacf1b9114bb915b5825679f4204d73fa934
S3TH375 <>
2026-04-21 20:07:26 +0000

    Challenge: Retrieving Letters from String

---
+++ section/challenge_retrieving_letters_from_string/main.py
@@ -0,0 +1,16 @@
+# Initial string to demonstrate string manipulation
+sample_text = "This is a string for learning"
+
+# Extracting the first character from the string
+___ = ___
+
+# Extracting the last character from the string
+___ = ___
+
+# Extracting a highlighted word from the string
+___ = ___
+
+# Testing
+print("The first char of the string: ", first_char)
+print("The last char of the string: ", last_char)
+print("The highlighted word from the string: ", highlighted_word)
